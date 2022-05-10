60f0aec7c0a1429735b33000c1404a7da98d4165
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-05-10 19:50:13 +0800

    optimize code

--- PeachBoxCounter.java
+++ PeachBoxCounter.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class PeachBoxCounter {
-    public void counter(int qty) {
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+
+        System.out.print("Enter the number of peaches picked: ");
+        int qty = get.nextInt();
+
         for(int x = 0; x <= (qty / 20); x++) {
             System.out.println("shipped " + (x * 20) + " peaches so far");
         }
+    
     }
 }
