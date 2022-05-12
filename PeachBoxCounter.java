5530b59ff10667eebf6b26ba8e7d048b295d4e67
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-05-12 13:10:05 +0800

    added exception handling

--- PeachBoxCounter.java
+++ PeachBoxCounter.java
@@ -1,14 +1,19 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PeachBoxCounter {
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
 
-        System.out.print("Enter the number of peaches picked: ");
-        int qty = get.nextInt();
+        try {
+            System.out.print("Enter the number of peaches picked: ");
+            int qty = get.nextInt();
 
-        for(int x = 0; x <= (qty / 20); x++) {
-            System.out.println("shipped " + (x * 20) + " peaches so far");
+            for(int x = 0; x <= (qty / 20); x++) {
+                System.out.println("shipped " + (x * 20) + " peaches so far");
+            }
+        } catch(InputMismatchException e) {
+            System.out.println(e);
         }
     
     }
